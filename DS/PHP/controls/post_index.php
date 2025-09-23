6a6a7f8eda2d8a6345b12258700becc02e9cfa4e
KAUE OLIVEIRA <kaueoliveira190409@gmail.com>
2025-09-23 12:27:32 -0300

    sda

--- DS/PHP/controls/post_index.php
+++ DS/PHP/controls/post_index.php
@@ -3,6 +3,7 @@ if ($_POST){
     require_once 'funcoes.php';
 
     if (isset($_POST['btnLogin'])){
+        echo "teste";
         $email = $_POST['txtEmail'];
         $senha = $_POST['txtSenha'];
         Tratamento::LoginExists($email,$senha);
--- DS/PHP/models/Config.php
+++ DS/PHP/models/Config.php
@@ -3,6 +3,6 @@ class connection {
     public static $host = "localhost";
     public static $user = "root";
     public static $password = "root";
-    public static $database = "db_vitoria_lanches";
+    public static $database = "bd_vitoria_lanches";
 }
 ?>
